003ebbe07e6610fc2e2fd786e48e1feec3b066a9
Vedant Agarwal <agarwalvedant02@gmail.com>
2026-02-24 00:23:25 +0530

    prod updates

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9fhiyme!e0h+!=w=qh@2q4%@hdij4$ap30bjmbkne9=%w)0(3d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
